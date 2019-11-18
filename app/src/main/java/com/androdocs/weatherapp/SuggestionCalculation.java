a604a5867ee1088b36e84150e8aaa0654079c8d3
eritcheygmu <55561283+eritcheygmu@users.noreply.github.com>
2019-11-17 21:53:30 -0500

    All issues worked out. Just the viewpager is off format wise.

--- app/src/main/java/com/androdocs/weatherapp/SuggestionCalculation.java
+++ app/src/main/java/com/androdocs/weatherapp/SuggestionCalculation.java
@@ -22,18 +22,18 @@ public class SuggestionCalculation {
     private ArrayList<String> allClothes = new ArrayList<>();
 
     public SuggestionCalculation() {
-        allClothes.add("T-Shirt"); // > 60              1
-        allClothes.add("Long Sleeve Shirt"); // <= 60   2
-        allClothes.add("Shorts"); // >60                3
-        allClothes.add("Pants"); // <= 60               4
-        allClothes.add("Umbrella"); //Raining / Misty   5
-        allClothes.add("Sunglasses"); //Sunny           6
-        allClothes.add("Jacket"); //Snowing, <= 40      7
-        allClothes.add("Sweatshirt"); //<= 50           8
-        allClothes.add("Snow Cap"); //Snowing           9
-        allClothes.add("Hat"); //Sunny                  10 As in "baseball cap"
-        allClothes.add("Gloves"); // Snowing            11
-        allClothes.add("Sweater"); //<=40               12
+        allClothes.add("T-Shirt"); // > 60              0
+        allClothes.add("Long Sleeve Shirt"); // <= 60   1
+        allClothes.add("Shorts"); // >60                2
+        allClothes.add("Pants"); // <= 60               3
+        allClothes.add("Umbrella"); //Raining / Misty   4
+        allClothes.add("Sunglasses"); //Sunny           5
+        allClothes.add("Jacket"); //Snowing, <= 40      6
+        allClothes.add("Sweatshirt"); //<= 50           7
+        allClothes.add("Snow Cap"); //Snowing           8
+        allClothes.add("Hat"); //Sunny                  9 As in "baseball cap"
+        allClothes.add("Gloves"); // Snowing            10
+        allClothes.add("Sweater"); //<=40               11
     }
 
     public String[] suggestion(int temp_int, String windSpeed, String weatherDescription){
@@ -45,54 +45,52 @@ public class SuggestionCalculation {
 
         //Various decisions based on weather data
         if(weatherDescription.equals("shower rain") || weatherDescription.equals("rain") || weatherDescription.equals("mist")){
-            suggestion_list.add(allClothes.get(5)); //Adds Umbrella
+            suggestion_list.add(allClothes.get(4)); //Adds Umbrella
             suggestionSize++;
         }
 
         if(weatherDescription.equals("clear skies")){
-            suggestion_list.add(allClothes.get(6)); //Adds Sunglasses
-            suggestion_list.add(allClothes.get(10)); //Adds Hat
+            suggestion_list.add(allClothes.get(5)); //Adds Sunglasses
+            suggestion_list.add(allClothes.get(9)); //Adds Hat
             suggestionSize+= 2;
         }
 
         if(weatherDescription.equals("snow")){
-            suggestion_list.add(allClothes.get(9)); //Adds Snow Cap
-            suggestion_list.add(allClothes.get(11));//Adds Gloves
-            suggestion_list.add(allClothes.get(7));//Adds Jacket
+            suggestion_list.add(allClothes.get(8)); //Adds Snow Cap
+            suggestion_list.add(allClothes.get(10));//Adds Gloves
+            suggestion_list.add(allClothes.get(6));//Adds Jacket
             suggestionSize+= 3;
         }
 
         if( !(weatherDescription.equals("snow")) && temp_int <= 40){
-            suggestion_list.add(allClothes.get(7));//Adds Jacket if not snowing but still under 40
+            suggestion_list.add(allClothes.get(5));//Adds Jacket if not snowing but still under 40
             suggestionSize++;
         }
 
         if(temp_int > 40 && temp_int <= 55){
-            suggestion_list.add(allClothes.get(8));//Adds Sweatshirt
+            suggestion_list.add(allClothes.get(7));//Adds Sweatshirt
             suggestionSize++;
         }
 
-        /*
         if(temp_int > 60){
-            suggestion_list.add(allClothes.get(1)); //Adds T-Shirt if over 60
+            suggestion_list.add(allClothes.get(0)); //Adds T-Shirt if over 60
             suggestionSize++;
         }
         else if (temp_int > 40 && temp_int <= 60){
-            suggestion_list.add(allClothes.get(2)); //Adds Long Sleeve Shirt if under or equal to 60 and greater than 40
+            suggestion_list.add(allClothes.get(1)); //Adds Long Sleeve Shirt if under or equal to 60 and greater than 40
             suggestionSize++;
         }
-        else{
-            suggestion_list.add(allClothes.get(12)); //adds sweater if greater than 40
+        else if(temp_int <= 40){
+            suggestion_list.add(allClothes.get(11)); //adds sweater if greater than 40
             suggestionSize++;
         }
-        */
 
         if(temp_int > 70){
-            suggestion_list.add(allClothes.get(3)); //Adds shorts if over 70
+            suggestion_list.add(allClothes.get(2)); //Adds shorts if over 70
             suggestionSize++;
         }
         else{
-            suggestion_list.add(allClothes.get(4)); //Adds pants if under or equal to 70
+            suggestion_list.add(allClothes.get(3)); //Adds pants if under or equal to 70
             suggestionSize++;
         }
 
